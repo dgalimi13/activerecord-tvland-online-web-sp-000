c11dbe6289def8fc3010d5e1ef0852aa3dd85130
danny galimi <dannygalimi13@aim.com>
2021-02-27 04:26:28 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -1,9 +1,7 @@
 class AddColumnsToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string 
-    add_column :shows, :day, :string 
-    
-      t.string :name
-    end
+    add_column :shows, :genre, :string 
+    add_column :shows, :season, :string 
   end
 end
